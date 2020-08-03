1d3d2c013e4b2fef4bfb53deeb445532c54ab03b
aboXmsa3d <aboXmsa3d@outlook.com>
2020-08-03 03:43:47 +0300

    create the models

---
+++ models.py
@@ -0,0 +1,68 @@
+#----------------------------------------------------------------------------#
+# Imports
+#----------------------------------------------------------------------------#
+
+import os
+from flask import Flask
+from sqlalchemy import Column, String, Integer, create_engine
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from datetime import datetime
+import json
+
+#----------------------------------------------------------------------------#
+# App Config.
+#----------------------------------------------------------------------------#
+
+database_name = "capstone"
+database_path = "postgres://{}:{}@{}/{}".format('postgres', 'd7oom11','localhost:5432', database_name)
+
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+#    db.create_all()
+    migrate = Migrate(app, db)
+
+#----------------------------------------------------------------------------#
+# Models.
+#----------------------------------------------------------------------------#
+
+# Association Object
+class movies_actors(db.Model):
+    __tablename__ = 'movies_actors'
+
+    movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'), primary_key=True)
+    actor_id = db.Column(db.Integer, db.ForeignKey('actor.id'), primary_key=True)
+
+    actor = db.relationship("Actor", back_populates="movies")
+    movie = db.relationship("Movie", back_populates="actors")
+
+   
+class Movie(db.Model):
+    __tablename__ = 'movie'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String() , nullable=False) 
+    release_date = db.Column(db.DateTime , default=datetime.utcnow) 
+
+    actors = db.relationship("movies_actors", back_populates="movie")
+
+
+class Actor(db.Model):
+    __tablename__ = 'actor'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String() , nullable=False)
+    age  = db.Column(db.Integer, nullable=False)
+    gender = db.Column(db.String(120) , nullable=False)
+
+    movies = db.relationship("movies_actors", back_populates="actor")
+
