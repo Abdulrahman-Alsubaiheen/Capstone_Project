e2c43e3c01b325b28c25709115477c455d4122ff
aboXmsa3d <aboXmsa3d@outlook.com>
2020-08-03 20:43:59 +0300

    App Config

--- app.py
+++ app.py
@@ -1,12 +1,89 @@
+#----------------------------------------------------------------------------#
+# Imports
+#----------------------------------------------------------------------------#
+
 import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from models import *
+
+#----------------------------------------------------------------------------#
+# App Config
+#----------------------------------------------------------------------------#
 
 def create_app(test_config=None):
+
   # create and configure the app
   app = Flask(__name__)
-  CORS(app)
+  setup_db(app)  
+  cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+  # CORS Headers 
+  @app.after_request
+  def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
+
+
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ endpoints ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+
+  @app.route('/', methods=['GET'])
+  def home():
+    
+      return jsonify({
+          'success': True,
+          'Wellcome': "Wellcome",
+      })
+
+
+
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Error Handlers ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+  @app.errorhandler(404)
+  def not_found(error):
+      return jsonify({
+          'success': False,
+          'error': 404,
+          'message': 'resource not found'
+      }), 404
+
+  @app.errorhandler(422)
+  def unprocessable(error):
+      return jsonify({
+          'success': False,
+          'error': 422,
+          'message': 'unprocessable'
+      }), 422
+
+  @app.errorhandler(400)
+  def bad_request(error):
+      return jsonify({
+          'success': False,
+          'error': 400,
+          'message': 'bad request'
+      }), 400
+
+  @app.errorhandler(405)
+  def not_allowed(error):
+      return jsonify({
+          'success': False,
+          'error': 405,
+          'message': 'method not allowed'
+      }), 405
+
+  @app.errorhandler(500)
+  def server_error(error):
+      return jsonify({
+        'success': False,
+        'error': 500,
+        'message': 'Internal server error',
+      }), 500
+
+
 
   return app
 
